14cc74a6628bbf27d5902f28dc6dc96b9dacf2a2
adkhiker <poirier.greg@gmail.com>
2019-10-21 19:20:36 -0400

    WIP: Adding recipe

--- secret-recipes/src/actions/index.js
+++ secret-recipes/src/actions/index.js
@@ -1,22 +1,36 @@
-// import axios from 'axios' 
-import { axiosWithAuth } from '../utils/axiosWithAuth';
+// import axios from 'axios'
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-export const FETCHING_RECIPES_START = "FETCHING_RECIPES_START"
-export const FETCHING_RECIPES_SUCCESS = "FETCHING_RECIPES_SUCCESS"
-export const FETCHING_RECIPES_FAIL = "FETCHING_RECIPES_FAIL"
+export const FETCHING_RECIPES_START = "FETCHING_RECIPES_START";
+export const FETCHING_RECIPES_SUCCESS = "FETCHING_RECIPES_SUCCESS";
+export const FETCHING_RECIPES_FAIL = "FETCHING_RECIPES_FAIL";
 
 export const getRecipes = token => dispatch => {
   // localStorage.setItem('token', token)
-  dispatch({ type: FETCHING_RECIPES_START })
+  dispatch({ type: FETCHING_RECIPES_START });
   axiosWithAuth()
-    .get('https://lambdaschool-cookbook2.herokuapp.com/recipes', {
+    .get("https://lambdaschool-cookbook2.herokuapp.com/recipes", {
       headers: {
         Authorization: token
       }
-    }
-    )
-    .then(res => {
-      console.log("something", res)
-      dispatch({ type: FETCHING_RECIPES_SUCCESS, payload: res.data })
     })
-}
+    .then(res => {
+      console.log("something", res);
+      dispatch({ type: FETCHING_RECIPES_SUCCESS, payload: res.data });
+    });
+};
+
+export const ADD_RECIPE_START = "ADD_RECIPE_START";
+export const ADD_RECIPE_SUCCESS = "ADD_RECIPE_SUCCESS";
+export const ADD_RECIPE_FAIL = "ADD_RECIPE_FAIL";
+
+export const addRecipe = newRecipe => dispatch => {
+  dispatch({ type: ADD_RECIPE_START });
+  axiosWithAuth()
+    .post("https://lambdaschool-cookbook2.herokuapp.com/recipes", newRecipe)
+    .then(res => {
+      console.log(res);
+      // dispatch({ type: ADD_RECIPE_SUCCESS, payload: res.data })
+    })
+    .catch(err => console.log(err));
+};
