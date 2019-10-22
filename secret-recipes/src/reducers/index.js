c12e9c766c2a9ebbe3cdeb5b0d2150089ae14d2b
Megan Jeffcoat <44781690+meganjeffcoat@users.noreply.github.com>
2019-10-22 00:01:18 -0500

    Revert "Adding Delete/Update"

--- secret-recipes/src/reducers/index.js
+++ secret-recipes/src/reducers/index.js
@@ -1,14 +1,17 @@
 import {
   FETCHING_RECIPES_START,
   FETCHING_RECIPES_SUCCESS,
+  FETCHING_RECIPES_FAIL,
   ADD_RECIPE_START,
-  ADD_RECIPE_SUCCESS
-} from "../actions/index";
+  ADD_RECIPE_SUCCESS,
+}
+
+  from '../actions/index';
 
 const initialState = {
   recipes: [],
   isFetching: false
-};
+}
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
@@ -16,25 +19,25 @@ export const reducer = (state = initialState, action) => {
       return {
         ...state,
         isFetching: true
-      };
+      }
     case FETCHING_RECIPES_SUCCESS:
       return {
         ...state,
         isFetching: false,
         recipes: action.payload
-      };
+      }
     case ADD_RECIPE_START:
       return {
         ...state,
         isFetching: true
-      };
+      }
     case ADD_RECIPE_SUCCESS:
       return {
         ...state,
         recipes: [...state.recipes, action.payload],
         isFetching: false
-      };
+      }
     default:
-      return state;
+      return state
   }
-};
+}
