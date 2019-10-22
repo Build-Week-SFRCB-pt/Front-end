9525b40642778b6c9c2ac273661d67203d3b39a4
Stephenfre <freemanstephen37@gmail.com>
2019-10-21 20:12:41 -0700

    Stephenfre

--- secret-recipes/src/App.js
+++ secret-recipes/src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import Header from "./components/RecipeCards/Header.js";
 import { BrowserRouter } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import RecipeList from "./components/Recipes/RecipeList";
 import PrivateRoute from "./utils/PrivateRoute";
 import AddRecipe from "./components/Recipes/AddRecipe.js";
-import LaunchPage from "./components/LaunchPage.js";
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
-        {/* <Route exact path="/" component={LaunchPage} /> */}
         <Route exact path="/" component={Register} />
         <Route exact path="/login" component={Login} />
         <PrivateRoute exact path="/recipes" component={RecipeList} />
